3f71805f95030c4b3bd936f2d88bd1b52d946001
이성재 <81102977+sungjae0309@users.noreply.github.com>
2026-08-12 21:32:25 +0900

    [FIX] 벨 알림 점이 벨 안쪽으로 너무 깊게 들어간 것을 살짝 되돌림

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -120,7 +120,7 @@ export default function HomePage() {
             <NotificationDot
               show={hasUnreadNotification}
               ring="gray-25"
-              className="top-[-1px] right-[1px] h-[11px] w-[11px]"
+              className="top-[-1px] right-[-1px] h-[11px] w-[11px]"
             />
           </span>
         </button>
